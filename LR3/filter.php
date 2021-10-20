e9057587700b216455f17af2c481429af756d9e6
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2021-10-20 15:22:15 +0300

    Update filter.php

--- LR3/filter.php
+++ LR3/filter.php
@@ -5,16 +5,6 @@ if($_SESSION['check'] !=1){
     header('location: autorization.php' );
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="icon" href="/imgall/img/unnamed.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "cssfiles/main1.css" type="text/css" >
-    <title>Выбер могил</title>
-</head>
-<body class = "position-relative">
 
 <? include('header.php');?>
 <div class = "p-4 container d-flex flex-column justify-content-center">
