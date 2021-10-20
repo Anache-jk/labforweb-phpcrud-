424ef8ffc3ad32624eaf1322265744956a16f391
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2021-10-20 15:19:10 +0300

    Update registration.php

--- LR3/registration.php
+++ LR3/registration.php
@@ -1,17 +1,7 @@
 <?php
 require_once 'vendor/signup.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="icon" href="/imgall/img/unnamed.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "cssfiles/main1.css" type="text/css" >
-    <title>Регистрация</title>
-</head>
-    <? include('header.php');?>
-<body>
+   <? include('header.php');?>
 <div class = "d-flex container-xxl justify-content-center p-4 ">
     <form action="" method = "post" class="form">
 
