71e4d74b005f8ae45433c1ccfe80f46bbcb00fbc
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2021-10-20 15:24:20 +0300

    Update autorization.php

--- LR3/autorization.php
+++ LR3/autorization.php
@@ -2,17 +2,8 @@
 error_reporting(E_ERROR | E_PARSE);
 require_once 'vendor/signin.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="icon" href="/imgall/img/unnamed.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <link rel = "stylesheet" href = "cssfiles/main1.css" type="text/css" >
-    <title>Авторизация</title>
-</head>
+
     <? include('header.php');?>
-<body class = "position-relative">
 <div class = "d-flex container-xxl justify-content-center">
 <form class="form" action="" method ="post">
     <label class = "labels">Ваш логин(email)</label>
