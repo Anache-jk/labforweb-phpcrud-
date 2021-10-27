93242a81526af8e2554cd3b9eed5ec9bee2bbb27
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2021-10-27 21:17:58 +0300

    Update logictext.php

--- LR1/logictext.php
+++ LR1/logictext.php
@@ -2,9 +2,9 @@
 error_reporting(E_ERROR | E_PARSE);
 require_once 'functionstext.php';
 $text = $_POST['textcheck'];
-$path1 = $_SERVER['DOCUMENT_ROOT'].'/wikirinhi.html';
-$path2 = $_SERVER['DOCUMENT_ROOT'].'/winnipuf.html';
-$path3 = $_SERVER['DOCUMENT_ROOT'].'/ukarainedialog.html';
+$path1 = 'wikirinhi.html';
+$path2 = 'winnipuf.html';
+$path3 = 'ukarainedialog.html';
 $wikitext = file_get_contents($path1);
 $winnitext = file_get_contents($path2);
 $ukrainetext = file_get_contents($path3);
