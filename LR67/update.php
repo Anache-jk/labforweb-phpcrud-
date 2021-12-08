ff491be2ce8b5c7f3667caee1f59bae0368f0479
Anache-jk <83112485+Anache-jk@users.noreply.github.com>
2021-12-09 00:25:00 +0300

    Update update.php

--- LR67/update.php
+++ LR67/update.php
@@ -2,8 +2,6 @@
 require_once 'vendor/logicviewtable.php';
 $humanid = htmlspecialchars($_GET['id']);
 $data = $obj->getById($humanid);
-if(!$data){ die("У пользователя некорректный id"); }
-$row = $obj->getplaces();
 ?>
 <? include('header.php');?>
     <div class = "d-flex container-xxl justify-content-center p-4 ">
